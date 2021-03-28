220b6de84cc2561e004fa59bd84ef18bec97aea1
bc <brendan@colloran.net>
2021-03-28 15:15:18 -0700

    intial commit

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import {terser} from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+
+const config = (file, plugins) => ({
+    input: 'index.js',
+    output: {
+        name: 'Delaunator',
+        format: 'umd',
+        indent: false,
+        file
+    },
+    plugins
+});
+
+export default [
+    config('delaunator.js', [resolve()]),
+    config('delaunator.min.js', [resolve(), terser()])
+];
